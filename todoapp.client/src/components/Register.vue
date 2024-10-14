e1d90d3d40e210c1f07706db36d618c8b5ff7283
avitsara <avitsarajary@yahoo.com>
2024-10-14 11:07:45 +0300

    register form front-end

--- todoapp.client/src/components/Register.vue
+++ todoapp.client/src/components/Register.vue
@@ -1,7 +1,9 @@
 <template>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
-</head>
+  </head>
   <div class="container mt-5">
     <div class="row justify-content-center">
       <div class="col-md-6">
@@ -142,5 +144,8 @@
 </script>
 
 <style scoped>
-
+  
+  input {
+   width : 500px;
+  }
 </style>
